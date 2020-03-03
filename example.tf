3d9fd073651325aed9aa7f0708a58d5dd09ceff7
apardo04 <apardo04@gmail.com>
2020-03-03 10:48:19 -0500

    testing terraform with github actions

---
+++ example.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  profile = "terraform"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
