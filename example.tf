ccb7e4e406d05990ac23abf282a540824beec1eb
apardo04 <apardo04@gmail.com>
2020-03-03 10:59:13 -0500

    reformatting tf

--- example.tf
+++ example.tf
@@ -1,16 +1,16 @@
 provider "aws" {
   profile = "terraform"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-b374d5a5"
+  ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
 
 resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
+  vpc      = true
+  instance = aws_instance.example.id
 }
 
 output "ip" {
