ea87ab77cf350f9657c62888b028d7cc71922bf6
apardo04 <apardo04@gmail.com>
2020-03-03 14:40:22 -0500

    reverting back to a working state; but doesn't communicate with TF Cloud

--- example.tf
+++ example.tf
@@ -1,12 +1,18 @@
-terraform {
-  backend "remote" {
+data "terraform_remote_state" "example" {
+  backend = "remote"
+
+  config = {
     hostname     = "app.terraform.io"
     organization = "apardo04"
-
-    workspaces {
-      prefix = "Example-Workspace"
+    token        = var.remote_state_token
+    workspaces = {
+      name = "Example-Workspace"
     }
   }
+}
+
+variable "remote_state_token" {
+  type = string
 }
 
 provider "aws" {
